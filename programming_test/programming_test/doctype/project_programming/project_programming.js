d0b774a468b06b30d96e897303571d871aac9a8f
Anas Al-Dharei <anas202255@gmail.com>
2023-11-26 02:20:40 +0530

    test second

---
+++ programming_test/programming_test/doctype/project_programming/project_programming.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, My Team and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Project Programming', {
+	// refresh: function(frm) {
+
+	// }
+});
